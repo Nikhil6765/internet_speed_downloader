a4bb390ff9db310c8d545f55e7a75c24f9d62fd6
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-05-07 12:54:35 +0530

    added_function

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import tkinter as tk 
 from PIL import Image,ImageTk
+import pyspeedtest 
+import math
+from tkinter import messagebox
+
 
 
 root=tk.Tk()
 root.geometry("300x450")
 root.resizable(0,0)
 root.title("Internet Speed Downloader")
+# creating function
+
+st=pyspeedtest.SpeedTest("www.google.com")
+def SpeedTest():
+    speed=str(math.floor(st.download()/1000)) + "kb/s"
+    messagebox.showinfo("The speed is ", speed)
+
+
+
+
 
 #logo
 
@@ -20,10 +34,10 @@ new_label.pack(padx=20,pady=20)
 
 # creating buttons
 
-button1=tk.Button(root, text="check",font=("Areal",16,"bold"))
+button1=tk.Button(root, text="check",command=SpeedTest,font=("Areal",16,"bold"))
 button1.pack(padx=20,pady=10)
 
-button1=tk.Button(root, text="exit",font=("Areal",16,"bold"))
+button1=tk.Button(root, text="exit",command=root.destroy,font=("Areal",16,"bold"))
 button1.pack(padx=10,pady=10)
   
 new_label2=tk.Label(root, text="Thanks !!", font=("Areal",16,"bold"),bg="black",fg="white" )
