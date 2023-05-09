29f3a6dfffd8a7b95d1a6bc2ba3a781b94462faa
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-05-09 19:11:31 +0530

    added

--- main.py
+++ main.py
@@ -18,24 +18,24 @@ def SpeedTest():
     messagebox.showinfo("The Speed is ", speed)
 
 #logo
-logo=Image.open("robot.png")
+logo=Image.open("logo.png")
 logo=ImageTk.PhotoImage(logo)
 logo_label=tk.Label(image=logo)
 logo_label.image=logo
 logo_label.pack()
 
-new_label=tk.Label(root, text="Test download speed",font=("Areal",18,"bold"), fg="green" )
+new_label=tk.Label(root, text="Test Download Speed",font=("Areal",18,"bold"), fg="green" )
 new_label.pack(padx=20,pady=20)
 
 # creating buttons
 
-button1=tk.Button(root, text="check",command=SpeedTest,font=("Areal",16,"bold"))
+button1=tk.Button(root, text="Check",command=SpeedTest,font=("Areal",16,"bold"))
 button1.pack(padx=20,pady=10)
 
-button2=tk.Button(root, text="exit",command=root.destroy,font=("Areal",16,"bold"))
+button2=tk.Button(root, text="Exit",command=root.destroy,font=("Areal",16,"bold"))
 button2.pack(padx=10,pady=10)
   
-new_label2=tk.Label(root, text="Thanks !!", font=("Areal",16,"bold"),bg="black",fg="white" )
+new_label2=tk.Label(root, text="Thanks !!", font=("Areal",22,"bold"),bg="black",fg="white" )
 new_label2.pack(padx=10,pady=10, fill="both", expand=True)
 
 root.mainloop()
